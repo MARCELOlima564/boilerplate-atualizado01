76171cef064a744c89afec421dd8a8cd02cc4f4b
Marcelo01 <marcelo.lima4@aluno.senai.br>
2024-08-28 13:26:10 -0300

    styles: add styles o home and totoro

--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,14 +1,16 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 
+import styles from './styles';
+
 export default function Totoro() {
     const navigation = useNavigation();
   return (
-    <View>
-      <Text>My Neighbor Totoro</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Neighbor Totoro</Text>
 
       <TouchableOpacity onPress={()=> navigation.navigate("Home")}>
-        <Text>Go to Home</Text>
+        <Text style={styles.text}>Go to Home</Text>
       </TouchableOpacity>
     </View>
   );
