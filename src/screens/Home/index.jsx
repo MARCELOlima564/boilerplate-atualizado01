16baba5d0eb2ff9c2e98b452465dc3aeab0f42a6
Marcelo01 <marcelo.lima4@aluno.senai.br>
2024-08-28 12:58:56 -0300

    feat: create screens navigation

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,9 +1,16 @@
 import { View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native-web';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>Home Screen</Text>
+
+      <TouchableOpacity onPress={()=> navigation.navigate("Totoro")}>
+        <Text>Go to Totoro</Text>
+      </TouchableOpacity>
     </View>
   );
 }
